f575d1fa2e49080f8f0e8549dde73850160fcd86
dcapwell <ekaqu1028 (at) gmail.com>
2012-07-28 23:29:21 -0700

    Added Lua function that can do http rather than needing to write java code

--- src/test/resources/NetworkCall.lua
+++ src/test/resources/NetworkCall.lua
@@ -8,3 +8,34 @@
 
 google = remoteGet("http://google.com")
 yahoo = remoteGet("http://google.com")
+
+-- Lua and LuaJ don't come with built in http support
+-- This method is a wrapper around URL.openStream
+function httpget(source)
+    url = luajava.newInstance( "java.net.URL", source )
+    sb = luajava.newInstance( "java.lang.StringBuilder" )
+
+    reader = luajava.newInstance("java.io.BufferedReader", luajava.newInstance("java.io.InputStreamReader", url:openStream()))
+    str = reader:readLine()
+    while (str) do
+        sb:append(str):append("\n")
+        str = reader:readLine()
+    end
+    closeables = luajava.bindClass("com.google.common.io.Closeables")
+    closeables.closeQuietly(reader)
+
+    return sb:toString()
+end
+
+function awsmetadata(key)
+    return httpget("http://169.254.169.254/latest/meta-data/" .. key)
+end
+
+
+
+aws = {
+    availabilityzone = awsmetadata("placement/availability-zone")
+}
+
+
+print(aws.availabilityzone)
